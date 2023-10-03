6326719cabdd29fc81913d05ae0b5113cf0a4237
ellis <ellis@rwest.io>
2023-10-02 21:50:10 -0400

    on the cli grind

--- cli.lisp
+++ cli.lisp
@@ -1,17 +1,17 @@
 (defpackage :organ.cli
-  (:use :cl :organ :macs.sym :macs.cli)
+  (:use :cl :organ :macs :cli :sym :log)
   (:export :*opts* :*cmds* :*cli* :main))
 
 (in-package :organ.cli)
 
-(defvar *opts* (make-opts '(:name version)
-			  '(:name log :global t)
-			  '(:name input)
-			  '(:name output)
-			  '(:name config)))
+(defvar *opts* (make-opts 
+		 (:name debug :global t)
+		 (:name help :global t)
+		 (:name version :global t)))
 
-(defvar *cmds* (make-cmds '(:name show)
-			  '(:name parse)))
+(defvar *cmds* (make-cmds 
+		 (:name show :opts (make-opts (:name input)))
+		 (:name parse :opts (make-opts (:name input) (:name output)))))
 
 (defvar *cli* 
   (make-cli t :name "organ"
@@ -21,8 +21,13 @@
 	      :cmds *cmds*))
 
 (defun run ()
-  (with-cli (opts cmds) *cli*
-    (print-help *cli*)))
+  (with-cli (opts cmds args) *cli*
+    (when (cli-val (find-opt *cli* "debug")) (setq *log-level* :debug))
+    (when (cli-val (find-opt *cli* "help")) (print-help *cli*))
+    (when (cli-val (find-opt *cli* "version")) (print-version *cli*))
+    (debug! (cli-opts *cli*) (cli-cmd-args *cli*) (cli-cmds *cli*))
+    (when-let ((a (cli-cmd-args (find-cmd *cli* "show"))))
+      (print a))))
 
 (defmain ()
   (run)
